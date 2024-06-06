b2b467ee1ca8acb22a5d7b97f34b0ef3ca8a19de
yang <zhiyang520679@gmail.com>
2024-06-06 11:24:06 +0200

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -180,10 +180,11 @@ const clear = ()=>{
     background-color: #fff;
 
     .fondo {
-        background: url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
-            url('@/assets/login_bg.jpeg') no-repeat center / cover;
-        border-radius: 0 20px 20px 0;
-    }
+    background: url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+                url('@/assets/login_bg.jpeg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+}
+
 
     .formulario {
         display: flex;
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -12,8 +11,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-  ,
+  },
   server: {
     proxy: {
       '/api': { // Obteniendo datos de la API
