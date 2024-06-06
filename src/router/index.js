9b4774ef55b32a002cfd07709291d3aa48f11232
yang <zhiyang520679@gmail.com>
2024-06-06 13:15:25 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-// Importar todas las rutas
+// Importar todas las vistas
 import LoginVue from '@/views/Login.vue'
 import LayoutVue from '@/views/Layout.vue'
 
@@ -15,17 +15,21 @@ import OrderManage from '@/views/order/OrderManage.vue'
 
 // Definir las rutas
 const routes = [
-    {   path: '/login',component: LoginVue},
-    {   path: '/',component: LayoutVue,redirect:'/login',children:[
-        {path:'/home',component:HomeMainVue},
-        {path:'/product/category',component:ProductCategoryVue},
-        {path:'/product/manage',component:ProductManageVue},
-        {path:'/user/info',component:UserInfoVue},
-        {path:'/user/avatar',component:UserAvatarVue},
-        {path:'/user/resetPassword',component:UserResetPasswordVue},
-        {path:'/offer/manage',component:OfferManage},
-        {path:'/order/manage',component:OrderManage},
-    ]}
+    { path: '/login', component: LoginVue, meta: { title: 'Iniciar sesión' } },
+    {
+        path: '/',
+        component: LayoutVue,
+        children: [
+            { path: '/home', component: HomeMainVue, meta: { title: 'Página principal' } },
+            { path: '/product/category', component: ProductCategoryVue, meta: { title: 'Categoría de productos' } },
+            { path: '/product/manage', component: ProductManageVue, meta: { title: 'Gestión de productos' } },
+            { path: '/user/info', component: UserInfoVue, meta: { title: 'Información de usuario' } },
+            { path: '/user/avatar', component: UserAvatarVue, meta: { title: 'Avatar de usuario' } },
+            { path: '/user/resetPassword', component: UserResetPasswordVue, meta: { title: 'Restablecer contraseña' } },
+            { path: '/offer/manage', component: OfferManage, meta: { title: 'Gestión de ofertas' } },
+            { path: '/order/manage', component: OrderManage, meta: { title: 'Gestión de pedidos' } },
+        ]
+    }
 ]
 
 // Crear el router
@@ -33,6 +37,12 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+// Establecer el título de la página en cada cambio de ruta
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title || 'Título predeterminado';
+    next();
+});
 
 // Exportar el router
 export default router
